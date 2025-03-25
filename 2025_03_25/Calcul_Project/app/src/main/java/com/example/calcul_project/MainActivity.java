2be997f463c0aec742371d7a82f717c9db4cad99
Moon ji hun <jihun0948@naver.com>
2025-03-25 14:20:09 +0900

    25.03.25

--- 2025_03_25/Calcul_Project/app/src/main/java/com/example/calcul_project/MainActivity.java
+++ 2025_03_25/Calcul_Project/app/src/main/java/com/example/calcul_project/MainActivity.java
@@ -1,8 +1,6 @@
-package com.example.dice_project;
+package com.example.calcul_project;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,44 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import java.util.Random;
-
 public class MainActivity extends AppCompatActivity {
-
-    ImageView imageView1, imageView2;
-
-
-    int[] diceImages = {
-            R.drawable.dice_1,
-            R.drawable.dice_2,
-            R.drawable.dice_3,
-            R.drawable.dice_4,
-            R.drawable.dice_5,
-            R.drawable.dice_6
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-
-        imageView1 = findViewById(R.id.imageView1);
-        imageView2 = findViewById(R.id.imageView2);
-
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
-
-    public void Roll_Dice(View view) {
-        Random rand = new Random();
-        int dice1 = rand.nextInt(6);
-        int dice2 = rand.nextInt(6);
-
-        imageView1.setImageResource(diceImages[dice1]);
-        imageView2.setImageResource(diceImages[dice2]);
     }
 }
